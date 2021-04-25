59c249c2969c95a6b126fab0356d2d23e60b6041
IvanTsiupa <ivan@verlocal.com>
2021-04-25 22:30:23 +0300

    changed activity card component

--- components/activity-card/ActivityCard.js
+++ components/activity-card/ActivityCard.js
@@ -4,34 +4,41 @@ import styles from '../../styles/ActivityCard.module.css';
 
 const ActivityCard = ({ activity: { id, title, date, image, host, country, city, votes }, showRating = false }) => {
     const activityLocation = `${country}, ${city}`;
+
     const { firstName, lastName, avatar, id: hostId } = host;
-    const fullName = `${firstName} ${lastName}`;
+    const hostFullName = `${firstName} ${lastName}`;
+    
     const dateOptions = { weekday: 'long', year: 'numeric', month: 'short', day: 'numeric' }
     const activityDate = new Intl.DateTimeFormat('en-US', dateOptions).format(new Date(date));
+    
+    const darkenedStyle = showRating ? styles.darkened : '';
 
     const countMeanRating = (votes) => {
         if(votes && Array.isArray(votes)){
             const userVotes = votes[0];
-            const ratings = Object.values(userVotes);
-            const ratingsSum = ratings.reduce((a, b) => a + b, 0);
-            return (ratingsSum / ratings.length).toFixed(2) || 0
+            if(userVotes){
+                const ratings = Object.values(userVotes);
+                const ratingsSum = ratings.reduce((a, b) => a + b, 0);
+                return (ratingsSum / ratings.length).toFixed(2) || 0
+            } else {
+                return 0;
+            }
         }
     }
 
-    const renderRating = (showRating, rating) => {
-        if(showRating){
-            return (
-                <span className={styles.activityRating}>{rating}</span>
-            )
-        }
+    const renderRating = (votes) => {
+        const rating = countMeanRating(votes);
+        return (
+            <span className={styles.activityRating}>{rating}</span>
+        )
     }
-
-    const darkenedStyle = showRating ? styles.darkened : ''
 
     return (
         <div className={styles.wrapper} data-activity-id={id}>
             <div className={`${styles.rollover} ${darkenedStyle}`}>
-                { renderRating(showRating, countMeanRating(votes)) }
+                { 
+                    showRating && renderRating(votes) 
+                }
                 <div className={styles.imageBox}>
                     <Link href={`/activities/${encodeURIComponent(id)}`}>
                         <a><img src={image} alt={title} className={`${styles.image}`} height="298"/></a>
@@ -50,12 +57,12 @@ const ActivityCard = ({ activity: { id, title, date, image, host, country, city,
                 </div>
                 <div className={styles.author}>
                     <div className={styles.authorIcon}>
-                        <img src={avatar} alt={fullName} />
+                        <img src={avatar} alt={hostFullName} />
                     </div>
                     <div className={styles.authorName}>
                         <span style={{ fontWeight: 500 }}>By&nbsp;</span>
                         <span className={styles.name}>
-                            <Link href={`/profiles/${encodeURIComponent(hostId)}`}><a>{fullName}</a></Link>
+                            <Link href={`/profiles/${encodeURIComponent(hostId)}`}><a>{hostFullName}</a></Link>
                         </span>
                     </div>
                 </div>
